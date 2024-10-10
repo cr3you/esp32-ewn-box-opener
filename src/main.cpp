9462a86568f6b172bb5c5d8b82c1f15e2d9c1063
Britcoin-Coder <78276611+Britcoin-Coder@users.noreply.github.com>
2024-10-10 12:36:46 +0100

    Update main.cpp
    Removed Creds

--- src/main.cpp
+++ src/main.cpp
@@ -22,13 +22,13 @@
 TFT_eSPI    tft = TFT_eSPI();
 
 //=====wifi setup
-const char *ssid = "BDA Home 2"; // <---------------------- SET THIS !!!
-const char *password = "D@taflow99"; // <-------------- SET THIS !!!
+const char *ssid = ""; // <---------------------- SET THIS !!!
+const char *password = ""; // <-------------- SET THIS !!!
 
 //=====Box Opener client setup
 const char *apiUrl = "https://api.erwin.lol/"; // mainnet
 //const char *apiUrl = "https://devnet-api.erwin.lol/"; // devnet
-const char *apiKey = "e05b6057f7706c64ffc1798c9ef4b75fca9f20ae489c07062597dc166a0b1cf854cd7144104727fb3ef1f1240e35d6ccb9b9fb257b189138189d0a7bb77d71285df2bce39dd6d6e45db72ab48f3971a468caac8d5c85900865c801199c6b375524b3dea6b41fe449b855cd3b571e9bc88f948da42d89e17790ca73b27effcef0"; // <---------------------- SET THIS !!!
+const char *apiKey = ""; // <---------------------- SET THIS !!!
 
 
 const int numGuesses = 50;
